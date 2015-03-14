ef0ba95bfbf6c0c8e4a1ebd59db9b963bd1bc9e0
RuZman <zoltan@ruzman.de>
2015-03-14 18:43:59 +0100

    Implemented some login features

--- src/main/java/de/ruzman/kurswahl/controller/LoginController.java
+++ src/main/java/de/ruzman/kurswahl/controller/LoginController.java
@@ -2,10 +2,17 @@ package de.ruzman.kurswahl.controller;
 
 import static io.datafx.controller.context.ApplicationContext.getInstance;
 import static javafx.collections.FXCollections.observableArrayList;
+import static javafx.scene.paint.Color.BLACK;
+import static javafx.scene.paint.Color.RED;
 import io.datafx.controller.FXMLController;
 import io.datafx.controller.context.ApplicationContext;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import javax.annotation.PostConstruct;
 
@@ -16,32 +23,87 @@ public class LoginController {
 	private ApplicationContext context = getInstance();
 	private DBJahrgang dbJahrgang;
 
-	@FXML private ComboBox<String> dropDownCourse;
-	@FXML private ComboBox<String> dropDownName;
-	@FXML private ComboBox<String> dropDownBirthDay;
-	@FXML private ComboBox<String> dropDownBirthMonth;
-	@FXML private ComboBox<String> dropDownBirthYear;
+	@FXML private Label courseLabel;
+	@FXML private Label nameLabel;
+	@FXML private Label birthDateLabel;
+	@FXML private Label birthPlace1Label;
+	@FXML private Label birthPlace2Label;
+
+	@FXML private ComboBox<String> courseComboBox;
+	@FXML private ComboBox<String> nameComboBox;
+	@FXML private ComboBox<String> birthDayComboBox;
+	@FXML private ComboBox<String> birthMonthComboBox;
+	@FXML private ComboBox<String> birthYearComboBox;
+
+	@FXML private TextField birthPlaceTextfield;
+
+	@FXML private Button loginButton;
 
 	@PostConstruct
 	public void initialize() {
 		dbJahrgang = context.getRegisteredObject(DBJahrgang.class);
-		dropDownCourse.setItems(observableArrayList(dbJahrgang.gibKurse()));
-		dropDownBirthDay.setItems(observableArrayList("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
-				"12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
-				"29", "30", "31"));
-		dropDownBirthMonth.setItems(observableArrayList("01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
-				"11", "12"));
-		dropDownBirthYear.setItems(observableArrayList(dbJahrgang.gibJahre()));
 
-		dropDownCourse.getItems().add(0, "- Auswählen -");
-		dropDownName.getItems().add(0, "- Auswählen -");
+		courseComboBox.getItems().addAll(observableArrayList(dbJahrgang.gibKurse()));
+		courseComboBox.valueProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				if (isSelected(courseComboBox)) {
+					resetTextFill();
+					dbJahrgang.gibSchueler().setzeKurs(newValue);
+					nameComboBox.getItems().remove(1, nameComboBox.getItems().size());
+					nameComboBox.getItems().addAll(dbJahrgang.gibNamen());
+				}
+				nameComboBox.setDisable(!isSelected(courseComboBox));
+				selectFirstItem(nameComboBox);
+			}
+		});
 
-		dropDownName.setDisable(true);
+		birthYearComboBox.setItems(observableArrayList(dbJahrgang.gibJahre()));
+		selectFirstItem(birthYearComboBox);
 
-		dropDownCourse.setValue(dropDownCourse.getItems().get(0));
-		dropDownName.setValue(dropDownName.getItems().get(0));
-		dropDownBirthMonth.setValue(dropDownBirthMonth.getItems().get(0));
-		dropDownBirthDay.setValue(dropDownBirthDay.getItems().get(0));
-		dropDownBirthYear.setValue(dropDownBirthYear.getItems().get(0));
+		loginButton.setOnAction((e) -> onLogin());
+
+		// TODO: Disable RED Color
+		// TODO: Use ActionTrigger
+	}
+
+	private void onLogin() {
+		// FIXME: Replace with CSS-Style
+		if (!isSelected(courseComboBox)) {
+			courseLabel.setTextFill(RED);
+		} else if (!isSelected(nameComboBox)) {
+			nameLabel.setTextFill(RED);
+		} else {
+			dbJahrgang.gibSchueler().setzeName(nameComboBox.getSelectionModel().getSelectedItem());
+			String birthDate = birthDayComboBox.getSelectionModel().getSelectedItem() + "."
+					+ birthMonthComboBox.getSelectionModel().getSelectedItem() + "."
+					+ birthYearComboBox.getSelectionModel().getSelectedItem();
+
+			if (dbJahrgang.gibSchueler().istGeburtsort(birthPlaceTextfield.getText())
+					&& dbJahrgang.gibSchueler().istGeburtsdatum(birthDate)) {
+				// TODO: Link to Kurswahl
+			} else {
+				birthDateLabel.setTextFill(RED);
+				birthPlace1Label.setTextFill(RED);
+				birthPlace2Label.setTextFill(RED);
+			}
+		}
+	}
+
+	private void resetTextFill() {
+		// TODO: Replace with CSS-Style
+		courseLabel.setTextFill(BLACK);
+		nameLabel.setTextFill(BLACK);
+		birthDateLabel.setTextFill(BLACK);
+		birthPlace1Label.setTextFill(BLACK);
+		birthPlace2Label.setTextFill(BLACK);
+	}
+
+	private boolean isSelected(ComboBox<String> comboBox) {
+		return comboBox.getSelectionModel().getSelectedIndex() != 0;
+	}
+
+	private void selectFirstItem(ComboBox<String> comboBox) {
+		comboBox.setValue(comboBox.getItems().get(0));
 	}
 }
